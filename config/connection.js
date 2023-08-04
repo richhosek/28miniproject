81113528dfe303ca12f83d66dcad4a174f5909be
Rich Hosek <richhosek@gmail.com>
2023-08-03 21:52:03 -0500

    first commit

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const { connect, connection } = require('mongoose');
+require("dotenv").config();
+// After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
+// Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb+srv://rhosek:Passw0rd@bootcamp.ztvhsdv.mongodb.net/';
+
+connect(connectionString);
+
+module.exports = connection;
